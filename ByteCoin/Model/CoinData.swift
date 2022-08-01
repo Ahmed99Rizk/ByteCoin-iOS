52004480ca80a258013e653b88f66e62a477b7bf
Ahmed99Rizk <49046761+Ahmed99Rizk@users.noreply.github.com>
2022-08-01 18:49:17 +0200

    Add files via upload

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Ahmed Rizk on 18/07/2022.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Codable {
+    let rate: Double
+}
